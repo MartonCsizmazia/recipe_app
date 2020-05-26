6d1a7097391e1e3d9a644f7301e1674e75e7f626
MartonCsizmazia <csmarton93@gmail.com>
2020-05-26 17:53:43 +0200

    fetching data from api working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-const App= () =>{
+const App = ()=>{
 
-    const APP_ID = "a43c5a10";
-    const APP_KEY = 'a8619d932136bcf4ff9de55a3ea4a7a7';
-//BACK TEXT ALTGR +7 `
-    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const APP_ID ='09f3fcff';
+    const APP_KEY ='cafd4efe65a8cab60caa05f23deabb4c';
 
-  return (
-    <div className="App">
-      <form className="search-form">
-          <input className="search-bar" type="text"/>
-          <button className="search-button" type="submit">Search</button>
-      </form>
-    </div>
-  );
-};
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(  ()=>{
+        getRecipes();
+    },[]);
+
+    const getRecipes = async()=>{
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`, {
+
+        });
+        const data = await  response.json();
+        setRecipes(data.hits);
+
+    }
+
+    return(
+        <div className="App">
+            <form className="search-form">
+                <input className="search-bar" type="text"/>
+                <button className="search-button"
+                        type="submit"
+                >
+                    Search
+                </button>
+            </form>
+        </div>
+
+    );
+}
 
 export default App;
